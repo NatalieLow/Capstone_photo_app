63703981d0480b65009541e04028dbf65c1aa3cf
Natalie Low <gnatann@gmail.com>
2020-01-28 14:18:53 -0800

    configured app databases

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,10 @@ module CapstoneApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    Mongoid.load!(".config/mongoid.yml")
+
+    config.generators {|g| g.orm :active_record}
+    
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true

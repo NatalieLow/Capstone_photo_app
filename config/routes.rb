9f7924fb0614c9f24df1a12b10879d1ee06236a0
Natalie Low <gnatann@gmail.com>
2020-01-29 10:10:12 -0800

    added root route under const

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  get '/ui/' => 'ui#index'
+  get '/ui#' => 'ui#index'
+  root "ui#index"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 

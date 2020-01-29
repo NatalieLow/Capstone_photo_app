560751fdac9174eb23baf56ee8fcf1e022e15689
Natalie Low <gnatann@gmail.com>
2020-01-29 09:45:48 -0800

    fixed bug in config

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module CapstoneApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    Mongoid.load!(".config/mongoid.yml")
+    Mongoid.load!("./config/mongoid.yml")
 
     config.generators {|g| g.orm :active_record}
     
